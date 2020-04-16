d0ce348dfc0775da1d5db1753d1287fef6314cc8
Jaehwan Ryu <jryu@uncharted.software>
2020-04-16 11:41:53 -0400

    Refactored ande moved tile to wm package

--- pkg/wm/elastic/tiles.go
+++ pkg/wm/elastic/tiles.go
@@ -3,46 +3,49 @@ package elastic
 import (
 	"fmt"
 
-	"github.com/paulmach/orb/geojson"
 	"gitlab.uncharted.software/WM/wm-go/pkg/wm"
 )
 
-// Tile is an individual tile from MaaS.
-// (TODO: I might need to move this to different place)
-type Tile struct {
-	zoom, x, y int
-	features   []*geojson.Feature
-}
+// // Tile is an individual tile from MaaS.
+// // (TODO: I might need to move this to different place)
+// type Tile struct {
+// 	zoom, x, y int
+// 	features   []*geojson.Feature
+// }
 
-// GetBounds returns tile bounds
-func (t *Tile) GetBounds() {
-	fmt.Println("NYI")
-}
+// // GetBounds returns tile bounds
+// func (t *Tile) Bound() wm.Bound {
+// 	return wm.Bound{
+// 		wm.Point{1.1, 2.2},
+// 		wm.Point{},
+// 	}
+// 	// fmt.Println("NYI")
+// }
 
-// AddFeatures loads geo features to the tile
-func (t *Tile) AddFeatures() {
-	fmt.Println("NYI")
-}
+// // AddFeatures loads geo features to the tile
+// func (t *Tile) AddFeatures() {
+// 	fmt.Println("NYI")
+// }
 
-// ToMVT returns the tile as mapbox vector tile format
-func (t *Tile) ToMVT() (string, error) {
-	return fmt.Sprintf("%d/%d/%d", t.zoom, t.x, t.y), nil
-}
+// // ToMVT returns the tile as mapbox vector tile format
+// func (t *Tile) ToMVT() (string, error) {
+// 	return fmt.Sprintf("%d/%d/%d", t.zoom, t.x, t.y), nil
+// }
 
-// NewTile creates a new tile
-func NewTile(zoom int, x int, y int) *Tile {
-	var features []*geojson.Feature
-	return &Tile{
-		zoom,
-		x,
-		y,
-		features,
-	}
-}
+// // NewTile creates a new tile
+// func NewTile(zoom int, x int, y int) Tile {
+// 	var features []*geojson.Feature
+// 	return Tile{
+// 		zoom,
+// 		x,
+// 		y,
+// 		features,
+// 	}
+// }
 
 // GetTile returns the tile.
 func (es *ES) GetTile(zoom int, x int, y int, specs wm.TileDataSpecs) (wm.Tile, error) {
-	tile := NewTile(zoom, x, y)
+	tile := wm.NewTile(zoom, x, y)
 
 	// TODO: get model output data for each of the TileDataSpec,
 	// combine them in to geojson features, and add the geo features(and their values) to the tile
--- pkg/wm/tile.go
+++ pkg/wm/tile.go
@@ -1,5 +1,11 @@
 package wm
 
+import (
+	"fmt"
+
+	"github.com/paulmach/orb/geojson"
+)
+
 // TileDataSpecs is a list of TileDataSpecs
 type TileDataSpecs []TileDataSpec
 
@@ -12,9 +18,45 @@ type TileDataSpec struct {
 	ValueProp string `json:"valueProp"`
 }
 
-// Tile interface
-type Tile interface {
-	GetBounds()
-	AddFeatures()
-	ToMVT() (string, error)
+// Tile is an individual tile from MaaS.
+type Tile struct {
+	zoom, x, y int
+	features   []*geojson.Feature
 }
+
+// Bound returns tile bounds
+func (t *Tile) Bound() {
+	// return orb.Bound{
+	// 	orb.Point{1.1, 2.2},
+	// 	orb.Point{1.2, 2.3},
+	// }
+	fmt.Println("NYI")
+}
+
+// AddFeatures loads geo features to the tile
+func (t *Tile) AddFeatures() {
+	fmt.Println("NYI")
+}
+
+// ToMVT returns the tile as mapbox vector tile format
+func (t *Tile) ToMVT() (string, error) {
+	return fmt.Sprintf("%d/%d/%d", t.zoom, t.x, t.y), nil
+}
+
+// NewTile creates a new tile
+func NewTile(zoom int, x int, y int) Tile {
+	var features []*geojson.Feature
+	return Tile{
+		zoom,
+		x,
+		y,
+		features,
+	}
+}
+
+// // Tile interface
+// type Tile interface {
+// 	Bound() Bound
+// 	AddFeatures()
+// 	ToMVT() (string, error)
+// }
