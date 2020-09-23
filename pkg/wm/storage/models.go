704691a644a23d80a16a034c09a516a7663a3ea3
Jaehwan Ryu <jryu@uncharted.software>
2020-09-23 12:03:57 -0400

    Epand subtiles

---
+++ pkg/wm/storage/models.go
@@ -0,0 +1,50 @@
+package storage
+
+// Model names
+const (
+	APSIM                 = "apsim"
+	AssetWealth           = "asset_wealth_model"
+	CHIRPS                = "chirps"
+	CLEM                  = "clem"
+	Consumption           = "consumption_model"
+	Cropland              = "cropland_model"
+	DSSAT                 = "dssat"
+	GRange                = "g-range"
+	FloodIndex            = "flood_index_model"
+	FSC                   = "fsc"
+	LPJML                 = "lpjml"
+	LPJMLHistoric         = "lpjml_historic"
+	Malnutrition          = "malnutrition_model"
+	MarketPrice           = "market_price_model"
+	MultiTwist            = "multi_twist"
+	PIHM                  = "pihm"
+	Population            = "population_model"
+	WorldPopulationAfrica = "world_population_africa"
+	YieldAnomaliesLPJML   = "yield_anomalies_lpjml"
+)
+
+var modelMaxPrecision = map[string]uint32{
+	GRange:       10,
+	Consumption:  14,
+	AssetWealth:  14,
+	Malnutrition: 15,
+	MarketPrice:  15,
+	PIHM:         16,
+
+	// Note: could not figure out or test max precision for following modles since we don't have them in our es currently
+	// Update theses accordingly when models are available in the data bas:w
+	// Might want to look at https://gitlab.uncharted.software/WM/wm-maas-ingest/blob/master/pkg/maas/model/resolutions.go for model resolution
+	APSIM:                 99,
+	CHIRPS:                99,
+	CLEM:                  99,
+	Cropland:              99,
+	DSSAT:                 99,
+	FloodIndex:            99,
+	FSC:                   99,
+	LPJML:                 99,
+	LPJMLHistoric:         99,
+	MultiTwist:            99,
+	Population:            99,
+	WorldPopulationAfrica: 99,
+	YieldAnomaliesLPJML:   99,
+}
--- pkg/wm/storage/tiles.go
+++ pkg/wm/storage/tiles.go
@@ -88,6 +88,13 @@ func (s *Storage) getRunOutput(zoom, x, y uint32, spec wm.TileDataSpec) (chan ge
 			return
 		}
 		timemillis := startTime.Unix() * 1000
+		modelMaxPrecision, ok := modelMaxPrecision[strings.ToLower(spec.Model)]
+		if !ok {
+			modelMaxPrecision = zoom + 5
+		}
+		maxZoom := modelMaxPrecision - 5
+		tileZoom := uint32(math.Min(float64(zoom), float64(maxZoom)))
+		fmt.Printf("zoom: %d, maxZoom: %d, tileZoom: %d\n", tileZoom, zoom, maxZoom)
 		key := fmt.Sprintf("%s/%s/%s/%d-%d-%d-%d.tile", strings.ToLower(spec.Model), spec.RunID, spec.Feature, timemillis, zoom, x, y)
 
 		// Retrieve protobuf tile from S3
@@ -99,8 +106,7 @@ func (s *Storage) getRunOutput(zoom, x, y uint32, spec wm.TileDataSpec) (chan ge
 		if err != nil {
 			if awsErr, ok := err.(awserr.Error); ok {
 				if awsErr.Code() == s3.ErrCodeNoSuchKey {
-					// Tile not found errors are expected
-					fmt.Printf("Key not found: %s\n", key)
+					// // Tile not found errors are expected
 					return
 				}
 				er <- err
@@ -124,21 +130,49 @@ func (s *Storage) getRunOutput(zoom, x, y uint32, spec wm.TileDataSpec) (chan ge
 		// Convert tile bin positions into z/x/y tile coordinates and save as geotiles
 		totalBins := tile.Bins.TotalBins
 		totalBinsXY := uint32(math.Pow(2, (math.Log(float64(totalBins)) / math.Log(4))))
+
+		// bin(subtile) precision
+		binPrecision := tile.Coord.Z + uint32(math.Log2(float64(totalBinsXY)))
+
+		// difference between the supported max precision for the model output and the bin(subtile) precision
+		var precisionDiff uint32
+
+		if binPrecision > modelMaxPrecision {
+			precisionDiff = binPrecision - modelMaxPrecision
+		}
+		type binAgg struct {
+			sum   float64
+			count uint32
+		}
+		tileMap := make(map[string]*binAgg)
 		var gts []geoTile
 		for binPosition, binStats := range tile.Bins.Stats {
-			gt := geoTile{
-				Key: fmt.Sprintf("%d/%d/%d",
-					tile.Coord.Z+uint32(math.Log2(float64(totalBinsXY))),
-					tile.Coord.X*totalBinsXY+uint32(math.Mod(float64(binPosition), float64(totalBinsXY))),
-					tile.Coord.Y*totalBinsXY+binPosition/totalBinsXY),
-				SpatialAggregation: geoTileAggregation{Value: binStats.Avg},
+			z := binPrecision
+			x := tile.Coord.X*totalBinsXY + uint32(math.Mod(float64(binPosition), float64(totalBinsXY)))
+			y := tile.Coord.Y*totalBinsXY + binPosition/totalBinsXY
+			if precisionDiff > 0 {
+				// Use precisionDiff level up parent tile coord
+				z = binPrecision - precisionDiff
+				x = x / (2 * precisionDiff)
+				y = y / (2 * precisionDiff)
 			}
-			gts = append(gts, gt)
+			coord := fmt.Sprintf("%d/%d/%d", z, x, y)
+			if _, ok := tileMap[coord]; !ok {
+				tileMap[coord] = &binAgg{}
+			}
+			tileMap[coord].sum += binStats.Avg
+			tileMap[coord].count++
 		}
 
-		// TODO: Get max zoom across all requested models and split up tiles on the fly to account
-		//       for models with different max zoom levels, an example of this is done in the
-		//       elasticsearch getRunOutput function
+		// Create geotiles
+		for coord, agg := range tileMap {
+			value := agg.sum / float64(agg.count)
+			gts = append(gts, geoTile{
+				Key:                coord,
+				SpatialAggregation: geoTileAggregation{Value: value},
+			})
+		}
+
 		wmTile := wm.NewTile(zoom, x, y, tileDataLayerName)
 		result := geoTilesResult{
 			bound: bound(wmTile.Bound()),
@@ -147,6 +181,9 @@ func (s *Storage) getRunOutput(zoom, x, y uint32, spec wm.TileDataSpec) (chan ge
 			data:  gts,
 		}
 		er <- nil
+		if precisionDiff > 0 {
+			result.data = subDivideTiles(result.data, binPrecision)
+		}
 		out <- result
 	}()
 	return out, er
@@ -171,4 +208,48 @@ func createFeatures(results []geoTilesResult) (map[string]geojson.Feature, error
 		}
 	}
 	return featureMap, nil
+}
+
+// subDivideTiles divides each tile of geoTiles into subdivided tiles at given precision and returns the result.
+// If tile precision(zoom level) of given tiles >= precision, just returns the original geoTiles
+func subDivideTiles(geoTiles []geoTile, precision uint32) []geoTile {
+	tiles := []geoTile{}
+	for _, geoTile := range geoTiles {
+		tiles = append(tiles, divideTile(geoTile, precision)...)
+	}
+	return tiles
+}
+
+// divideTile divides the tile into 4^level smaller subtiles
+// For given tile with zoom level 1 and 2 as level param, it will produce 16 (4^2) subtiles of zoom level 3
+func divideTile(tile geoTile, level uint32) []geoTile {
+	var z, x, y uint32
+	fmt.Sscanf(tile.Key, "%d/%d/%d", &z, &x, &y)
+
+	if level <= z {
+		return []geoTile{tile}
+	}
+	var tiles []geoTile
+	// Details on tile calculation: https://wiki.openstreetmap.org/wiki/Slippy_map_tilenames#Subtiles
+	topLeft := geoTile{
+		Key:                fmt.Sprintf("%d/%d/%d", z+1, 2*x, 2*y),
+		SpatialAggregation: tile.SpatialAggregation,
+	}
+	topRight := geoTile{
+		Key:                fmt.Sprintf("%d/%d/%d", z+1, 2*x+1, 2*y),
+		SpatialAggregation: tile.SpatialAggregation,
+	}
+	bottomLeft := geoTile{
+		Key:                fmt.Sprintf("%d/%d/%d", z+1, 2*x, 2*y+1),
+		SpatialAggregation: tile.SpatialAggregation,
+	}
+	bottomRight := geoTile{
+		Key:                fmt.Sprintf("%d/%d/%d", z+1, 2*x+1, 2*y+1),
+		SpatialAggregation: tile.SpatialAggregation,
+	}
+	tiles = append(tiles, divideTile(topLeft, level)...)
+	tiles = append(tiles, divideTile(topRight, level)...)
+	tiles = append(tiles, divideTile(bottomLeft, level)...)
+	tiles = append(tiles, divideTile(bottomRight, level)...)
+	return tiles
 }
