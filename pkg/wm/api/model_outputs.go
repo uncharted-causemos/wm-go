e7ebebf0021b627a631aa8db53514c23d33d9aff
Daniel Bruce <dbruce@uncharted.software>
2021-07-12 13:05:53 -0400

    use level not region key

--- pkg/wm/api/model_outputs.go
+++ pkg/wm/api/model_outputs.go
@@ -84,14 +84,14 @@ func (a *api) getModelOutputTimeseries(w http.ResponseWriter, r *http.Request) {
 func (a *api) getRegionalDataOutputStats(w http.ResponseWriter, r *http.Request) {
 	params := getDatacubeParams(r)
 	regionMap := make(wm.ModelRegionalOutputStat)
-	for i := 0; i < 4; i++ {
+	for i, level := range []string{"country", "admin1", "admin2", "admin3"} {
 		var regionKey = fmt.Sprintf("regional_level_%d_stats", i)
 		stats, err := a.dataOutput.GetOutputStats(params, regionKey)
 		if err != nil {
 			a.errorResponse(w, err, http.StatusInternalServerError)
 			return
 		}
-		regionMap[regionKey] = *stats
+		regionMap[level] = *stats
 	}
 
 	render.Render(w, r, &modelRegionalOutputStatsResponse{&regionMap})
